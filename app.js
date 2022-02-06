6dc943012720fd0c16c0d67d7e1f35ca8558e5de
uvi <you@example.com>
2022-02-06 00:36:47 -0800

    adding scripts for heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 80;
+const port = process.env.PORT || 80;
 const mongoose = require('mongoose');
 const bodyparser = require('body-parser')
 
